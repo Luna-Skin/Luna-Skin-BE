81f53b3da870c60c77a2d2b6ca4404578c511f6f
김민호 <2022305011@skuniv.ac.kr>
2026-08-05 18:18:07 +0900

    :sparkles: Feat: 채팅방 생성 기능 구현 (#6)

--- src/main/java/com/luna/skin/domain/chat/controller/ChatController.java
+++ src/main/java/com/luna/skin/domain/chat/controller/ChatController.java
@@ -1,16 +1,15 @@
 package com.luna.skin.domain.chat.controller;
 
+import com.luna.skin.domain.chat.dto.request.CreateChatRoomRequest;
 import com.luna.skin.domain.chat.dto.response.ChatRoomListResponse;
+import com.luna.skin.domain.chat.dto.response.CreateChatRoomResponse;
 import com.luna.skin.domain.chat.service.ChatService;
 import com.luna.skin.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +34,20 @@ public class ChatController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(BaseResponse.success(chatRoomList));
+    }
+
+    @PostMapping("/rooms")
+    @Operation(
+            summary = "채팅방 생성 컨트롤러입니다.",
+            description = "채팅방 식별자, 채팅방 제목, 채팅방 생성 시간을 반환합니다."
+    )
+    public ResponseEntity<BaseResponse<CreateChatRoomResponse>> createChatRoom(
+            @RequestParam Long userId,
+            @RequestBody CreateChatRoomRequest createChatRoomRequest
+    ) {
+        CreateChatRoomResponse createChatRoom = chatService.createChatRoom(userId, createChatRoomRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.success(createChatRoom));
     }
 
 }

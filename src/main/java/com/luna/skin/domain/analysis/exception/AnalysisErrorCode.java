40e2f1681b036026b00c2743007dd1661a5df160
{오지우} <ojing0116@skuniv.ac.kr>
2026-08-06 19:03:07 +0900

    :bug: Fix: 에러 코드에 webp 제거

--- src/main/java/com/luna/skin/domain/analysis/exception/AnalysisErrorCode.java
+++ src/main/java/com/luna/skin/domain/analysis/exception/AnalysisErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum AnalysisErrorCode implements ErrorCode {
-  INVALID_FILE_TYPE("ANALYSIS_400", "지원하지 않는 파일 형식입니다. (jpg, png, webp)", HttpStatus.BAD_REQUEST),
+  INVALID_FILE_TYPE("ANALYSIS_400", "지원하지 않는 파일 형식입니다. (jpg, jpeg, png)", HttpStatus.BAD_REQUEST),
   FILE_TOO_LARGE("ANALYSIS_400", "파일 크기는 10MB를 초과할 수 없습니다.", HttpStatus.BAD_REQUEST),
   EMPTY_FILE("ANALYSIS_400", "파일이 비어있습니다.", HttpStatus.BAD_REQUEST);
 

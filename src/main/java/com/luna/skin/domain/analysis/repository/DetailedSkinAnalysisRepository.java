035221240bf35fef1d49f41d4101ca1c94d43321
{오지우} <ojing0116@skuniv.ac.kr>
2026-08-09 02:16:31 +0900

    :sparkles: Feat: 생활습관 영향 분석 API 구현

--- src/main/java/com/luna/skin/domain/analysis/repository/DetailedSkinAnalysisRepository.java
+++ src/main/java/com/luna/skin/domain/analysis/repository/DetailedSkinAnalysisRepository.java
@@ -2,10 +2,13 @@ package com.luna.skin.domain.analysis.repository;
 
 import com.luna.skin.domain.analysis.entity.AiAnalysis;
 import com.luna.skin.domain.analysis.entity.DetailedSkinAnalysis;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface DetailedSkinAnalysisRepository extends JpaRepository<DetailedSkinAnalysis, Long> {
   Optional<DetailedSkinAnalysis> findByAiAnalysis(AiAnalysis aiAnalysis);
+
+  List<DetailedSkinAnalysis> findAllByAiAnalysisIn(List<AiAnalysis> analyses);
 }

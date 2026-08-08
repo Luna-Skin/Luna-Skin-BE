5f7ecbba0bba324c72f9ce5aac8b8ee7ad567e14
{오지우} <ojing0116@skuniv.ac.kr>
2026-08-09 01:32:47 +0900

    :sparkles: Feat: 일별 피부 분석 조회 API 구현 및 기존 피부분석 Response에 image url 필드 추가

--- src/main/java/com/luna/skin/domain/analysis/repository/DetailedSkinAnalysisRepository.java
+++ src/main/java/com/luna/skin/domain/analysis/repository/DetailedSkinAnalysisRepository.java
@@ -1,9 +1,11 @@
 package com.luna.skin.domain.analysis.repository;
 
+import com.luna.skin.domain.analysis.entity.AiAnalysis;
 import com.luna.skin.domain.analysis.entity.DetailedSkinAnalysis;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface DetailedSkinAnalysisRepository extends JpaRepository<DetailedSkinAnalysis, Long> {
+  Optional<DetailedSkinAnalysis> findByAiAnalysis(AiAnalysis aiAnalysis);
 }

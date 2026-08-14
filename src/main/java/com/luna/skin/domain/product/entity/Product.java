670b6fc10708c1d81742a9a5ba143596908544a0
김민호 <2022305011@skuniv.ac.kr>
2026-08-14 15:40:53 +0900

    :bug: Fix: product 엔티티 내 imageURL 삭제 & flyway 수정

--- src/main/java/com/luna/skin/domain/product/entity/Product.java
+++ src/main/java/com/luna/skin/domain/product/entity/Product.java
@@ -21,10 +21,4 @@ public class Product extends BaseTimeEntity {
 
     @Column(name = "purpose", length = 50)
     private String purpose;
-
-    @Column(name = "price")
-    private Integer price;
-
-    @Column(name = "image_url")
-    private String imageUrl;
 }
